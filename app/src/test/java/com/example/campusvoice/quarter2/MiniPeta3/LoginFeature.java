53b0bea26c6fe91f27fbc897637e49ccc3824222
SCA-Maniclang, Ahkira Clive <AhkiraManiclang12@gmail.com>
2026-09-18 15:10:45 +0800

    Login Feature: //Checking Login Info NEXT//

--- app/src/test/java/com/example/campusvoice/quarter2/MiniPeta3/LoginFeature.java
+++ app/src/test/java/com/example/campusvoice/quarter2/MiniPeta3/LoginFeature.java
@@ -1,4 +1,29 @@
 package com.example.campusvoice.quarter2.MiniPeta3;
 
+import java.util.Scanner;
 public class LoginFeature {
+
+   public static void main(String[] args){
+     Scanner input = new Scanner(System.in);
+
+     String registeredUsername;
+     String registeredPassword;
+
+     //LOGIN INTERFACE
+       System.out.println("=====LOGIN=====");
+
+       System.out.print("Username: ");
+       String username = input.nextLine();
+
+       System.out.print("Password: ");
+       String password = input.nextLine();
+
+       //Checking Login Info
+       if (username.equals(registeredUsername)
+
+
+
+
+   }
+
 }
