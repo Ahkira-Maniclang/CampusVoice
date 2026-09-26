497043d38560b40f901300e22b640676b06a5a70
Carla Rodriguez <cmpr063010@gmail.com>
2026-09-27 01:53:13 +0800

    Commit 1: Skeleton Setup by Rodriguez

---
+++ app/src/test/java/com/example/campusvoice/quarter2/PracticalExam/CinemaMenu.java
@@ -0,0 +1,11 @@
+package com.example.campusvoice.quarter2.PracticalExam;
+
+import java.util.Scanner;
+
+public class CinemaMenu {
+
+    public void start(Scanner scanner) {
+        // Menu logic will be added in the next checkpoint.
+    }
+}
+
